0f8e1eb6cf14b8758f7a840a66bde9e45d25bf12
Tom Arnfeld <tarnfeld@me.com>
2014-02-16 15:28:52 +0000

    Extract ALL the volunteers

--- 5-collect-volunteers/src/scraper.py
+++ 5-collect-volunteers/src/scraper.py
@@ -14,10 +14,11 @@ from collections import namedtuple
 
 
 # Define a bunch of regular expressions for parsing out data
-_VOLUNTEER_LATLONG_PARSER = re.compile(r'latLng:\[([0-9.\-]+),([0-9.\-]+)\]')
+_VOLUNTEER_MAP_LATLONG_PARSER = re.compile(r'latLng:\[([0-9.\-]+),([0-9.\-]+)\], data:.*?/offers/details/([0-9]+)')
+_VOLUNTEER_LATLONG_PARSER = re.compile(r'latLng:\[([0-9.\-]+),([0-9.\-]+)\],')
 
 # Define some structs
-Point = namedtuple('Point', ['lat', 'lng'])
+Point = namedtuple('Point', ['lat', 'lng', 'volunteer_id'])
 
 
 def scrape_volunteer_points(map_page_url):
@@ -29,7 +30,24 @@ def scrape_volunteer_points(map_page_url):
     if status != 200:
         raise Exception("Failed to load map page with status %r" % status)
 
-    for match in _VOLUNTEER_LATLONG_PARSER.finditer(r.text):
-        lat, lng = match.groups()
+    max_volunteer_id = None
+    for match in _VOLUNTEER_MAP_LATLONG_PARSER.finditer(r.text):
+        lat, lng, volunteer_id = match.groups()
+        max_volunteer_id = max(max_volunteer_id, int(volunteer_id))
 
-        yield Point(lat=float(lat), lng=float(lng))
+    for volunteer_id in xrange(1, max_volunteer_id + 1):
+        url = "%s/maps/offer_map/%d" % (map_page_url.rstrip("/"), volunteer_id)
+        r = requests.get(url)
+
+        status = r.status_code
+        if status != 200:
+            continue
+
+        match = _VOLUNTEER_LATLONG_PARSER.search(r.text)
+        if match:
+            lat, lng = match.groups()
+            yield Point(
+                lat=float(lat),
+                lng=float(lng),
+                volunteer_id=volunteer_id
+            )
